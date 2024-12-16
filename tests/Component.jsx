adf1bd562f7be8db0d55205160933b3e4e796fa8
Brad Garropy <bradgarropy@gmail.com>
2024-12-16 15:42:34 -0600

    update tests.

---
+++ tests/Component.jsx
@@ -0,0 +1,5 @@
+const Component = () => {
+    return <h1>😘</h1>
+}
+
+export default Component
--- tests/app.js
+++ tests/app.js
@@ -1,5 +1,3 @@
-import fs from "node:fs"
-
 const hello = () => {
     const subject = "world"
     console.log(`Hello ${subject}!`)
